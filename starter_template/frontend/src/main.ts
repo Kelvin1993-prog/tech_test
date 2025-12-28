0a337805831e891ed29e46baff0920b49cf2e7fb
Kelvin1993-prog <nnabugwukelvin1993@gmail.com>
2025-12-28 14:23:25 +0000

    Initial commit – Customer Insights Dashboard

--- starter_template/frontend/src/main.ts
+++ starter_template/frontend/src/main.ts
@@ -1,10 +1,10 @@
-/*
-*  Protractor support is deprecated in Angular.
-*  Protractor is used in this example for compatibility with Angular documentation tools.
-*/
-import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent,
-    {providers: [provideProtractorTestingSupport()]})
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+
+platformBrowserDynamic([
+  provideCharts(withDefaultRegisterables())
+])
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));
